2d54ebf0980b0c81cc54eda7e3ef1db209d6a25b
keyestudio <215650023@qq.com>
2025-01-16 10:29:36 +0800

    2025-01-16 10:29:36.36

--- conf.py
+++ conf.py
@@ -69,6 +69,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_book_theme'
+html_theme_options = {
+  "show_toc_level": 4
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
