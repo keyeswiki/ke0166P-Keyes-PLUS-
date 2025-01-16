9dc74a0227e2141adb6d95d1bc540dcfd25288c2
keyestudio <215650023@qq.com>
2025-01-16 13:50:32 +0800

    2025-01-16 13:50:32.27

--- conf.py
+++ conf.py
@@ -70,7 +70,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
-  "show_toc_level": 3
+  "show_toc_level": 3,
+  "show_navbar_depth": 2,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
